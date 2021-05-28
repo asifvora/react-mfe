fc52a700570320153cc476d1dba51c8ef8b304da
Asif Vora <asifvora0@gmail.com>
2021-05-28 19:19:24 +0530

    🔧  Update build configuration files

--- packages/frontend-components/feature-01/src/components/Alerts.tsx
+++ packages/frontend-components/feature-01/src/components/Alerts.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Dispatch } from 'redux';
 import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import { incriment, decrement } from '../actions/alerts';
-// import { Button } from '@mfe/shared/dist/bundle';
+import { Button } from '@mfe/shared/dist/bundle';
 
 export const Alerts: React.FC = () => {
   const dispatch: Dispatch<any> = useDispatch();
@@ -21,9 +21,9 @@ export const Alerts: React.FC = () => {
 
   return (
     <div>
-      <button  onClick={onIncriment}>+</button>
+      <Button primary onClick={onIncriment}>+</Button>
         <p>Alerts is :{count}</p>
-      <button  onClick={onDecrement}>-</button>
+      <Button primary onClick={onDecrement}>-</Button>
     </div>
   );
 };
