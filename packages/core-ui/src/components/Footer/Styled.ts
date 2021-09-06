983a5392b242bd39f281a7683fb938a80e9b2a24
asifvora <asifvora0@gmail.com>
2021-09-06 17:59:00 +0530

    :lipstick: Added style for loader

--- packages/core-ui/src/components/Footer/Styled.ts
+++ packages/core-ui/src/components/Footer/Styled.ts
@@ -1,26 +1,22 @@
 import styled from 'styled-components';
-import { Container } from '../../GlobalStyles';
 
 export const FooterSection = styled.section`
   display: flex;
   align-items: center;
   color: #fff;
   background-color: #333;
+  padding: 3rem;
 `;
 
-export const FooterContainer = styled(Container)`
+export const FooterContainer = styled.div`
   display: flex;
-  flex-direction: column;
   align-items: center;
-  padding: 1rem 1rem 3rem 2rem;
-  ${Container}
 `;
 
 export const FooterCopyRight = styled.a`
   text-decoration: none;
   outline: none;
   color: #fff;
-  margin-top: 3rem;
   &:hover {
     color: #e38b06;
   }
