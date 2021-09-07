96c6149184e76d12c442ed5a1b59b9498d989bc1
asifvora <asifvora0@gmail.com>
2021-09-07 14:35:18 +0530

    :wrench: Added support for PWA

--- packages/core-ui/craco.config.js
+++
@@ -1,9 +0,0 @@
-const CracoWorkboxPlugin = require('craco-workbox');
-
-module.exports = {
-  plugins: [
-    {
-      plugin: CracoWorkboxPlugin
-    }
-  ]
-};
--- packages/core-ui/src/index.tsx
+++ packages/core-ui/src/index.tsx
@@ -6,7 +6,7 @@ import { toast } from '@mfe/shared/dist/bundle';
 import { Provider } from 'react-redux'
 import store from './store'
 import GlobalStyles from './GlobalStyles';
-import * as serviceWorker from './service-worker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import './scss/index.scss';
 
@@ -22,13 +22,12 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
