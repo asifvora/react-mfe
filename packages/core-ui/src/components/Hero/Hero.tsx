b69d3ac5f74a26a53272682cd9ce373ac032156c
asifvora <asifvora0@gmail.com>
2021-09-10 15:45:14 +0530

    :sparkles: Adding support for dark theme

--- packages/core-ui/src/components/Hero/Hero.tsx
+++ packages/core-ui/src/components/Hero/Hero.tsx
@@ -11,24 +11,22 @@ import {
 
 export const Hero: React.FC = () => {
   return (
-    <div>
-      <HeroContainer>
-        <Navbar />
-        <HeroContent>
-          <HeroContentText>
-            <HeroTitle>
-              <HeroTitleText>Healthy</HeroTitleText>
-              <HeroTitleText>Meals All Day</HeroTitleText>
-            </HeroTitle>
-            <HeroSubTitle>For a longer Life</HeroSubTitle>
-            <HeroText>
-              Get a fresh and tasty recepies that are well balanced and will
-              improve your wellness, plus add nutrients to your body.
-            </HeroText>
-          </HeroContentText>
-        </HeroContent>
-      </HeroContainer>
-    </div>
+    <HeroContainer>
+      <Navbar />
+      <HeroContent>
+        <HeroContentText>
+          <HeroTitle>
+            <HeroTitleText>Healthy</HeroTitleText>
+            <HeroTitleText>Meals All Day</HeroTitleText>
+          </HeroTitle>
+          <HeroSubTitle>For a longer Life</HeroSubTitle>
+          <HeroText>
+            Get a fresh and tasty recepies that are well balanced and will
+            improve your wellness, plus add nutrients to your body.
+          </HeroText>
+        </HeroContentText>
+      </HeroContent>
+    </HeroContainer>
   );
 };
 
